e303b0ec07baef0b7ca2cbe29367ef476dd8db56
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-11-25 09:17:37 +0100

    By default, analyze the 'runtime classpath' of java projects

--- src/main/kotlin/io/fuchs/gradle/collisiondetector/CollisionDetectorPlugin.kt
+++ src/main/kotlin/io/fuchs/gradle/collisiondetector/CollisionDetectorPlugin.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.util.GradleVersion
 
+import org.gradle.api.plugins.JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME
+
 abstract class CollisionDetectorPlugin : Plugin<Project> {
 
     private val taskName: String = "detectCollisions"
@@ -14,9 +16,18 @@ abstract class CollisionDetectorPlugin : Plugin<Project> {
             throw GradleException("This plugin only supports Gradle 6.6 or newer versions")
         }
 
-        project.tasks.register(taskName, DetectCollisionsTask::class.java) {
+        val task = project.tasks.register(taskName, DetectCollisionsTask::class.java) {
             // convention useful for most Jars, can be replaced completely using 'setExcludes()'
             it.collisionFilter.exclude("META-INF/**", "module-info.class")
+        }
+
+        project.pluginManager.withPlugin("java") {
+            val runtimeClasspath = project.configurations.getByName(RUNTIME_CLASSPATH_CONFIGURATION_NAME)
+            task.configure {
+                // for standard Java projects: by default, analyze the runtime classpath
+                // can be replaced completely using 'setFrom'
+                it.configurations.from(runtimeClasspath)
+            }
         }
     }
 
